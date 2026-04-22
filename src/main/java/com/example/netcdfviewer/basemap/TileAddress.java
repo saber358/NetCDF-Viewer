ff2b723bc3e5adf7f2ac29879990bc1dabfec3f1
LWJ19386977562 <14959344+LWJ19386977562@user.noreply.gitee.com>
2026-04-22 14:56:17 +0800

    Revert "Add basemap tile definitions"
    This reverts commit cb844f79302b7b565d5f3a35bb6f40f5bf88d67c.

--- src/main/java/com/example/netcdfviewer/basemap/TileAddress.java
+++
@@ -1,9 +0,0 @@
-package com.example.netcdfviewer.basemap;
-
-public record TileAddress(int z, int x, int y) {
-    public TileAddress {
-        if (z < 0 || x < 0 || y < 0) {
-            throw new IllegalArgumentException("瓦片编号不能为负数。");
-        }
-    }
-}
