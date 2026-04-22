dd58220c120bba1878296bbeb9f7a44a2ff06905
LWJ19386977562 <14959344+LWJ19386977562@user.noreply.gitee.com>
2026-04-22 14:56:17 +0800

    Revert "Add basemap tile renderer"
    This reverts commit 988d5c3f0753576c66f9e1a1b17506b6b8fce48c.

--- src/main/java/com/example/netcdfviewer/basemap/BaseMapRenderResult.java
+++
@@ -1,9 +0,0 @@
-package com.example.netcdfviewer.basemap;
-
-import java.awt.image.BufferedImage;
-
-public record BaseMapRenderResult(BufferedImage image, String message) {
-    public static BaseMapRenderResult empty() {
-        return new BaseMapRenderResult(null, null);
-    }
-}
